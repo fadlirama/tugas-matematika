ca256f9b54e8fedbcb76a1ff12efaf1ede92424f
Fatan <fatanbasyari82@gmail.com>
2022-04-19 10:39:28 +0700

    tugas

---
+++ index.js
@@ -0,0 +1,16 @@
+// luas persegi
+function persegi(sisi){
+  return sisi * sisi
+}
+console.log(persegi(5));
+
+//persegi panjang
+function panjang(p,t){
+  return p * t
+}
+console.log(panjang(4,2));
+
+//lingkaran
+const luaslingkaran =(r) => {
+  const luas = 3.14 
+}
